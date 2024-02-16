62eb559ca1f26334a257ff83ac5b7d497239e11d
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-16 16:53:29 +0100

    ex 1/2 and 2b

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
-let numero = 42;
-console.log(numero);
+////// Exercise 1: Declare a variable and print its value in the console \\\\\
+let number = 100; // number
+console.log(number); // Print the value //
+
+////// Exercise 2: Declare a constant and print its value in the console \\\\\
+const PI = 3.14; // number (constant)
+console.log(PI); // Print the value of the constant
+
+////// Exercise 2/B: Attempt to reassign a value to a constant\\\\\
+// const PI  = 3.1416;
+// console.log(PI);
+// error because constants cannot be reassigned.
